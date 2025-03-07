cd07b7519f276b5adf27345a43727b9d60bfe9f5
Dmitry Surdin <dsurdin@eyeconweb.com>
2025-03-07 14:07:03 +0800

    add likes for posts

--- src/videos/validation.helpers.ts
+++ src/videos/validation.helpers.ts
@@ -1,45 +1,43 @@
 import { Resolution } from './video.types';
 
 export const isString = (value: unknown): value is string => {
-  return typeof value === 'string';
+	return typeof value === 'string';
 };
 
 export const isValidTitle = (title: string) => {
-  return isString(title) && !!title.trim() && title.length < 40;
+	return !!title.trim() && isString(title) && title.length < 40;
 };
 
 export const isValidAuthor = (author: string) => {
-  return isString(author) && !!author.trim() && author.length < 20;
+	return isString(author) && !!author.trim() && author.length < 20;
 };
 
 export const isValidResolution = (resolutions: Array<string> | null) => {
+	if (!resolutions) {
+		return false;
+	}
 
-  if (!resolutions) {
-    return false;
-  }
-
-  const resolutionValues = Object.values(Resolution);
-  return resolutions.every((resolution) => resolutionValues.includes(resolution as Resolution));
+	const resolutionValues = Object.values(Resolution);
+	return resolutions.every((resolution) => resolutionValues.includes(resolution as Resolution));
 };
 
 export const isValidCanBeDownloaded = (canBeDownloaded?: boolean) => {
-  return typeof canBeDownloaded === 'boolean';
+	return typeof canBeDownloaded === 'boolean';
 };
 
 export const isValidMinAgeRestriction = (minAgeRestriction?: number | null) => {
-  return (
-    (minAgeRestriction === null || (typeof minAgeRestriction === 'number' && minAgeRestriction >= 1 && minAgeRestriction <= 18))
-  );
+	return (
+		minAgeRestriction === null ||
+		(typeof minAgeRestriction === 'number' && minAgeRestriction >= 1 && minAgeRestriction <= 18)
+	);
 };
 
 export const isValidPublicationDate = (publicationDate?: string) => {
-  if (!publicationDate) {
-    return true;
-  }
+	if (!publicationDate) {
+		return true;
+	}
 
-  const isoFormatRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
+	const isoFormatRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
 
-  return (
-    !isNaN(new Date(publicationDate).getTime()) && isoFormatRegex.test(publicationDate)
-  );
+	return !isNaN(new Date(publicationDate).getTime()) && isoFormatRegex.test(publicationDate);
 };
